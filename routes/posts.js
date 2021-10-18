64d5e92ecfb55ec3d32c3896d5844a80a0fd9c8f
rohan <rohannak2057@gmail.com>
2021-10-18 20:27:50 +0530

    added post requests

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,62 @@
 const { route } = require("./users");
 
 const router = require("express").Router();
-router.get("/", async (req, res) => {
-    console.log("post");
-    res.send("post");
+const Post = require("../models/Post")
+// create a post
+router.post("/", async (req, res) => {
+    const newPost = new Post(req.body)
+    try {
+        const savedPost = await newPost.save();
+        res.status(200).json(savedPost);
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+})
+// update a post
+router.put("/:id", async (req, res) => {
+    try {
+        const post = Post.findById(req.params.id);
+        if (post.userId === req.body.userId) {
+            await post.updateOne({ $set: req.body });
+            res.status(200).json("Post Updated!");
+        }
+        else {
+            res.status(403).json("You can update only your post");
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
+// delete post
+router.delete("/:id", async (req, res) => {
+    try {
+        const post = Post.findById(req.params.id);
+        if (post.userId === req.body.userId) {
+            await post.deleteOne();
+            res.status(200).json("Post Deleted!");
+        }
+        else {
+            res.status(403).json("You can delete only your post");
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+//   like,dislike post
+router.put("/:id/like", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post.likes.include(req.body.userId)) {
+            await post.updateOne({ $push: { likes: req.body.userId } });
+            res.status(200).json("You liked this post");
+        } else {
+            await post.updateOne({ $pull: { likes: req.body.userId } });
+            res.status(200).json("Disliked");
+        }
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+})
 module.exports = router;
