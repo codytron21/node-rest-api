51bc32787cf4149f9affadb84ae0a427e4dc1688
rohan <rohannak2057@gmail.com>
2021-11-27 09:40:50 +0530

    added conversation and message routes

--- index.js
+++ index.js
@@ -11,20 +11,22 @@ const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
+const conversationRoute = require("./routes/conversations");
+const messageRoute = require("./routes/messages");
 //
 const multer = require("multer");
 //to prevent the restapi to make get request to the file location.
 const path = require("path");
-// to use 
+// to use
 dotenv.config();
 //to connect to mongodb using mongoose (making connection)
 mongoose
-    .connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log("Database connected!"))
-    .catch(err => console.log(err));
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connected!"))
+  .catch((err) => console.log(err));
 //it will prevent any request on /images instead it will go to public/images directory.
 app.use("/images", express.static(path.join(__dirname, "public/images")));
 //adding middleware
@@ -34,25 +36,24 @@ app.use(morgan("common"));
 
 // to indicate storage and directory for the upload files.
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "public/images");
-    },
-    filename: (req, file, cb) => {
-        //req.body.name contains the filename which we will send to client side. 
-        cb(null, req.body.name);
-    },
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    //req.body.name contains the filename which we will send to client side.
+    cb(null, req.body.name);
+  },
 });
 const upload = multer({ storage: storage });
 //writting a request here for uploading files.(we can do this by making separate routes like before . like we did for users ,posts)
 app.post("/api/upload", upload.single("file"), (req, res) => {
-    try {
-        //  upload.single() will automatically upload our file ,so we will just return the response.
-        return res.status(200).json("file uploaded successfully.");
-    }
-    catch (error) {
-        console.log(error);
-    }
-})
+  try {
+    //  upload.single() will automatically upload our file ,so we will just return the response.
+    return res.status(200).json("file uploaded successfully.");
+  } catch (error) {
+    console.log(error);
+  }
+});
 // using app
 // "/" means homepage.
 // app.get("/", (req, res) => {
@@ -61,7 +62,9 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute); //address for rest api,whenever we go to /api/users it is going to run userRoute.
 app.use("/api/posts", postRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
 
 app.listen(8800, () => {
-    console.log("Backend server is running");
+  console.log("Backend server is running");
 });
